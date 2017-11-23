89c2999c71f7f9e7656a67b23c1c889628129fee
AdrieneBatista3 <adriene_paula@estudante.sc.senai.br>
2017-11-23 22:39:40 +0000

    Terminando Sistema

--- app/models/aeronave.rb
+++ app/models/aeronave.rb
@@ -1,2 +1,14 @@
 class Aeronave < ActiveRecord::Base
+
+validates_presence_of :nome, on: :create, message: "Não pode ficar em branco"
+validates_presence_of :numero_de_tripulantes, on: :create, message: "Não pode ficar em branco"
+validates_presence_of :tamanho, on: :create, message: "Não pode ficar em branco"
+validates_presence_of :numero, on: :create, message: "Não pode ficar em branco"
+validates_presence_of :chassi, on: :create, message: "Não pode ficar em branco"
+validates_presence_of :quantidade_de_horas_de_voo, on: :create, message: "Não pode ficar em branco"
+validates_presence_of :quantidade_de_motor, on: :create, message: "Não pode ficar em branco"
+
+
+
+
 end
--- app/models/aluno.rb
+++ app/models/aluno.rb
@@ -1,2 +1,15 @@
 class Aluno < ActiveRecord::Base
+
+validates_presence_of :nome, on: :create, message: "Não pode ficar em branco"
+validates_presence_of :sobrenome, on: :create, message: "Não pode ficar em branco"
+validates_presence_of :cpf, on: :create, message: "Não pode ficar em branco"
+validates_presence_of :endereco, on: :create, message: "Não pode ficar em branco"
+validates_presence_of :profissao, on: :create, message: "Não pode ficar em branco"
+validates_presence_of :email, on: :create, message: "Não pode ficar em branco"
+validates_presence_of :cnh, on: :create, message: "Não pode ficar em branco"
+validates_presence_of :escolaridade, on: :create, message: "Não pode ficar em branco"
+validates_presence_of :instituicao, on: :create, message: "Não pode ficar em branco"
+validates_presence_of :curso, on: :create, message: "Não pode ficar em branco"
+
+
 end
