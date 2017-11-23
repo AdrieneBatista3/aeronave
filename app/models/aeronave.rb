adbb677e444d58fd2fa46e2feb2c790f0f1d3d87
AdrieneBatista3 <adriene_paula@estudante.sc.senai.br>
2017-11-23 23:21:05 +0000

    Terminando Sistema

--- app/models/aeronave.rb
+++ app/models/aeronave.rb
@@ -1,14 +1,13 @@
 class Aeronave < ActiveRecord::Base
 
-validates_presence_of :nome, on: :create, message: "Não pode ficar em branco"
-validates_presence_of :numero_de_tripulantes, on: :create, message: "Não pode ficar em branco"
-validates_presence_of :tamanho, on: :create, message: "Não pode ficar em branco"
-validates_presence_of :numero, on: :create, message: "Não pode ficar em branco"
-validates_presence_of :chassi, on: :create, message: "Não pode ficar em branco"
-validates_presence_of :quantidade_de_horas_de_voo, on: :create, message: "Não pode ficar em branco"
-validates_presence_of :quantidade_de_motor, on: :create, message: "Não pode ficar em branco"
 
-
+   validates_presence_of :nome, on: :create, message: " não pode ficar em branco"
+   validates_presence_of :numero_de_tripulantes, on: :create, message: " não pode ficar em branco"
+   validates_presence_of :tamanho, on: :create, message: " não pode ficar em branco"
+   validates_presence_of :numero, on: :create, message: " não pode ficar em branco"
+   validates_presence_of :chassi, on: :create, message: " não pode ficar em branco"
+   validates_presence_of :quantidade_de_horas_de_voo, on: :create, message: " não pode ficar em branco"
+   validates_presence_of :quantidade_de_motor, on: :create, message: " não pode ficar em branco"
 
 
 end
